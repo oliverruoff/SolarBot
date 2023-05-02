31f6d14f48ce8c703a15cf8d793c71d0a5d92092
Oliver Ruoff <oliver93ruoff@web.de>
2023-05-02 20:28:43 +0200

    adding text to frame

--- code/server.py
+++ code/server.py
@@ -91,6 +91,11 @@ def gen():
     while True:
         rval, frame = vc.read()
         frame = cv2.flip(frame, flipCode=-1)
+        text = "Test"
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+        text_x = frame.shape[1] - text_size[0] - 10
+        text_y = frame.shape[0] - text_size[1] - 10
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imwrite(tmp_img_path, frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + open(tmp_img_path, 'rb').read() + b'\r\n')
